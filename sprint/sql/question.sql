b572848d96cc1d3139cbc7b19ec3072c23ee18ac
elBichon <reuglewiczjeanedouard@gmail.com>
2016-08-08 13:46:44 +0200

    Update question.sql

--- sprint/sql/question.sql
+++ sprint/sql/question.sql
@@ -6,5 +6,6 @@ create table question (
 id integer not null primary key auto_increment,
 nomFormulaire varchar(50) not null,
 type varchar(50) not null,
-question varchar(50) not null
+question varchar (50) not null,
+qId varchar (50) not null
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
